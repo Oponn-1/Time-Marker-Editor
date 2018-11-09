7322bf0ce5f3c5cb1b54de9d5ecb4942eadd1f63
Andres De La Fuente Duran <33918646+Oponn-1@users.noreply.github.com>
2018-11-08 20:21:26 -0500

    Added documentation

--- TimingDocument.cs
+++ TimingDocument.cs
@@ -1,7 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+
+//      This contains a class which is used in my Time Editor, a custom Unity editor window to create
+//      timing information with respect to an audio file.
+//
+//      Author:     Andres De La Fuente Duran
+
+
+//******************************************* Class: TimingDocument  ***********************************************//
+//      
+//      Use:        A class to represent the timing information for an entire piece of music. This is 
+//                  used simply to represent the data in an object which can then be converted to JSON
+//                  for saving and loading.
+//
+//      Fields:
+//                  Public ......................................................................................
+//                  
+//                  timeMarks:  The list of time markers from the Time Editor window
+//
+//                  length:     The duration of the corresponding piece of music
+//
+
 
 [Serializable]
 public class TimingDocument {
